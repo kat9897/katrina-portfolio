727d7af91ce8e5c23555f2938dc78938a6a19414
Katrina <61071011+kat9897@users.noreply.github.com>
2021-03-01 13:51:52 -0500

    Build Start

---
+++ build/precache-manifest.ddcf5aa16f77ceeaf41420056579c757.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a73366435d3eb7d295dab4f2ec8f94d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19389d4ef0e2704c590a",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "0da85d7a666f48bb5312",
+    "url": "/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "006b4bfa8cc111a586b3",
+    "url": "/static/css/main.207db4cf.chunk.css"
+  },
+  {
+    "revision": "93d019b47e136b0b2455",
+    "url": "/static/js/2.533fe64b.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/static/js/2.533fe64b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19389d4ef0e2704c590a",
+    "url": "/static/js/3.ef5e8dbe.chunk.js"
+  },
+  {
+    "revision": "0da85d7a666f48bb5312",
+    "url": "/static/js/4.5225d0c5.chunk.js"
+  },
+  {
+    "revision": "006b4bfa8cc111a586b3",
+    "url": "/static/js/main.e4fd0363.chunk.js"
+  },
+  {
+    "revision": "bb568c459b700b7513c9",
+    "url": "/static/js/runtime-main.ff717ff7.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "feb6806bbb369da951f5112ba7968ee3",
+    "url": "/static/media/Crossing.feb6806b.jpg"
+  },
+  {
+    "revision": "686c5ba51512be2e1fa8c57b32b773db",
+    "url": "/static/media/Cybersecurity.686c5ba5.jpg"
+  },
+  {
+    "revision": "d778379eff916887b57e3c4addb09de1",
+    "url": "/static/media/GLO_Project.d778379e.jpg"
+  },
+  {
+    "revision": "e4a920d553bce2117a8d042bdb5ddc60",
+    "url": "/static/media/Helping_Hearts.e4a920d5.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "fc505e691befbb7ce217069e6d31fda8",
+    "url": "/static/media/One Of Us Final.fc505e69.png"
+  },
+  {
+    "revision": "dde8cf5433c96a597b305a72869be9dd",
+    "url": "/static/media/Seasoning_Shack.dde8cf54.jpg"
+  },
+  {
+    "revision": "375a5114b282ecdf7079b11a292e0184",
+    "url": "/static/media/Smart-Serve-Logo.375a5114.png"
+  },
+  {
+    "revision": "11ec10a5d0e2c70425e500399b47dd97",
+    "url": "/static/media/UTSC.11ec10a5.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "01ba63907dc99b0e2fd02ea9255ffe05",
+    "url": "/static/media/htv.01ba6390.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "8e25ed6df7d11f00adbde9308311163d",
+    "url": "/static/media/okami-kai.8e25ed6d.jpg"
+  },
+  {
+    "revision": "fac9ee8c103a7e4e0aab34709cde16db",
+    "url": "/static/media/safeTALK.fac9ee8c.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "d763e79d88538e973b97d8966a881b97",
+    "url": "/static/media/sky-chat-logo.d763e79d.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "b6e17d69f0ce33f89762e48ef3f041f3",
+    "url": "/static/media/theroxy.b6e17d69.png"
+  },
+  {
+    "revision": "f214f9521177d942f385371cc5801d05",
+    "url": "/static/media/tshirtganglogo.f214f952.png"
+  },
+  {
+    "revision": "dd839eb13b113a21b6db59d9c1aeb008",
+    "url": "/static/media/uxbridge_soccer.dd839eb1.jpg"
+  }
+]);
